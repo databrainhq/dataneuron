8d622ab8826337ad94dfc8588a47bd93a2344b14
Vysakh Sreenivasan <vysakh@pipesort.com>
2024-07-30 12:02:30 +0530

    add lambda deploy approach and expose functions

---
+++ src/data_neuron/lambda_handler.py
@@ -0,0 +1,28 @@
+import json
+from .server import app as application
+
+
+def lambda_handler(event, context):
+    with application.test_client() as client:
+        http_method = event['httpMethod']
+        path = event['path']
+        headers = event.get('headers', {})
+        query_string = event.get('queryStringParameters', {})
+        body = event.get('body', '')
+
+        if body:
+            body = json.loads(body)
+
+        response = client.open(
+            path,
+            method=http_method,
+            headers=headers,
+            query_string=query_string,
+            json=body
+        )
+
+        return {
+            'statusCode': response.status_code,
+            'body': response.data.decode('utf-8'),
+            'headers': dict(response.headers)
+        }
